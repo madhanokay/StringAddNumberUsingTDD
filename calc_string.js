daaaaca5c3ece85d9488680c2426c3fefe9486cf
madhan <m3madhankumar@gmail.com>
2025-04-25 22:03:13 +0530

    add delimiter core and test cases

--- calc_string.js
+++ calc_string.js
@@ -18,9 +18,26 @@ function add (numbers) {
     }
     return num;
   });
+  
+  //Delimiter core...
+  if (numbers.startsWith("//")) {
+    const delimiterEnd = numbers.indexOf("\n");
+    if (delimiterEnd < 0) {
+        throw new Error("Invalid delimiter format: Missing newline after delimiter definition.");
+    }
+    delimiter = numbers.substring(2, delimiterEnd);
+    numbersString = numbers.substring(delimiterEnd + 1);
+    if (delimiter === "") {
+        throw new Error("Delimiter cannot be empty");
+    }
+    // Escape special regex characters in the delimiter.  Important for user-defined delimiters.
+    delimiter = delimiter.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+  }
 
   // Calculate the sum
   return nums.reduce((sum, num) => sum + num, 0);
+
+  
 }
 
 // Core TestCase function...
@@ -54,10 +71,27 @@ function testMultipleNumbers() {
   checkTestCase(add("1,2,3,4,5"), 15, "Test Case 4: Multiple numbers");
 }
 
+// Test case 4: Multiple numbers
+function testMultipleNumbers() {
+  checkTestCase(add("1,2,3,4,5"), 15, "Test Case 4: Multiple numbers");
+}
+
+// Test case 5: Newline delimiter
+function testNewlineDelimiter() {
+  checkTestCase(add("1\n2,3"), 6, "Test Case 5: Newline delimiter");
+}
+
+// Test case 6: Different delimiter
+function testDifferentDelimiter() {
+  checkTestCase(add("//;\n1;2"), 3, "Test Case 6: Different delimiter");
+}
+
 // Run the tests....
 testEmptyString();
 testSingleNumber();
 testTwoNumbers();
 testMultipleNumbers();
+testNewlineDelimiter();
+testDifferentDelimiter();
 
 
